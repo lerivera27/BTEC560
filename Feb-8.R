ec0be134d904a2d06c42b73c07e5f1b78f312e99
Leiara <156393855+lerivera27@users.noreply.github.com>
2024-02-13 10:22:18 -0500

    Add files via upload

--- Feb-8.R
+++ Feb-8.R
@@ -9,3 +9,8 @@ my_data <- mtcars
 #Create a plot of your data 
 ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
 
+#Create a plot of your data (change xlab & ylab)
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", cor.method = "pearson", xlab = "Final Composition", ylab = "car weight")
+
+#Calculating the correlation test value 
+res <- cor.test(my_data$wt, my_data$mpg, method = "pearson")
