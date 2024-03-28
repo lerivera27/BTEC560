1a15a499e26d25d0b8a502957477f6c516315767
Leiara <156393855+lerivera27@users.noreply.github.com>
2024-03-28 10:18:38 -0400

    Add files via upload

---
+++ In-Class Assignment- 3:28.R
@@ -0,0 +1,10 @@
+#Author: Rivera, Date: March 26, 2024, Purpose: Test Different Distribution Functions 
+
+dbinom(4,size=12, prob=0.2)
+
+dbinom(4,size=12, prob=0.2)
+[1] 0.1328756
+
+dbinom(4,size=20, prob=0.1)
+[1] 0.08977883
+
