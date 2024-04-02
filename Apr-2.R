a31cbfe670deab1b9ff6311640615f9f049453fe
Leiara <156393855+lerivera27@users.noreply.github.com>
2024-04-02 10:17:26 -0400

    Add files via upload

---
+++ Apr-2.R
@@ -0,0 +1,30 @@
+#Author: Leiara Rivera, Date: April 2nd, 2024, Purpose: Test Different Distribution Functions
+
+
+#Poisson Distribution 
+ppois(16, lambda=12)
+[1] 0.898709
+
+#Uniform Distribution 
+runif(10, min=1, max=3)
+
+runif(10, min=1, max=3)
+ [1] 2.717633 2.719504 2.717322 2.513292 2.239964 2.581781 1.523177 1.288776 1.104344 2.846424
+
+#Chisquare Distribution 
+qchisq(.95,7)
+[1] 14.06714
+
+#T Distribution 
+qt(c(.025,.975), df=5)
+c=contain
+
+qt(c(.025,.975), df=5)
+[1] -2.570582  2.570582
+#two different percentiles mean two different intervals 
+
+#F Distribution 
+qf(.95,df1=5, df2=2)
+qf(.95,df1=5,df2=2)
+[1] 19.29641
+
